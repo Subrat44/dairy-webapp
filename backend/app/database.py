9daab4d86e5f9d386e6484558c41466c7c218a56
Sk Pradhan <kumarskp920@gmail.com>
2026-03-17 00:41:55 +0530

    feat: MongoDB Atlas connected, deployed to EC2

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
 DB_NAME   = os.getenv("DB_NAME", "dairyhub")
 
 class Database:
